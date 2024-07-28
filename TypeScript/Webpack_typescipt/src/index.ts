1b476693bdbb8f350e6e9660ad0480c51f662f10
Malavivikas <33936264+Malavivikas@users.noreply.github.com>
2024-07-28 10:04:07 +0530

    moved typescript folder outside of directory

--- TypeScript/Webpack_typescipt/src/index.ts
+++
@@ -1,3 +0,0 @@
-console.log('Hello');
-console.log('Hello World');
-console.log('Hello Vikas');
--- TypeScript/les6/public/sandbox.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const logDetails = (uid, item) => {
-    console.log(`${item} has a uid of ${uid}`);
-};
-logDetails(123, 'John');
-const greet = (user) => {
-    console.log(`${user.name} says hello`);
-};
-greet({ name: 'John', uid: 123 });
